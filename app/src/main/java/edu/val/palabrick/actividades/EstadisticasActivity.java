70656196e62500cddcf8127ef5d37b9acb00933c
valexx55 <valexx55@gmail.com>
2022-06-21 19:39:28 +0200

    GUARDANDO Y MOSTRANDO ESTADITICAS 1/2

--- app/src/main/java/edu/val/palabrick/actividades/EstadisticasActivity.java
+++ app/src/main/java/edu/val/palabrick/actividades/EstadisticasActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import edu.val.palabrick.R;
+import edu.val.palabrick.model.ResultadosEstadisticosJugador;
+import edu.val.palabrick.util.GestionPreferenciasUsuario;
 
 public class EstadisticasActivity extends AppCompatActivity {
 
@@ -23,9 +26,15 @@ public class EstadisticasActivity extends AppCompatActivity {
 
     private void initActividad()
     {
+        //TODO EN 10/15 MINUTOS
         //LEER DEL FICHERO DE PREFRENCIAS
+        ResultadosEstadisticosJugador rej= GestionPreferenciasUsuario.leerEstadísticasJugador(this);
         //Y ACTUALIZAR LA PATANLLA DE ESTADÍSTICAS
+        TextView partidas_jugadas = findViewById(R.id.partidas_jugadas);
+        TextView porcentaje_victorias = findViewById(R.id.porcentaje_victorias);
         //CON LOS DATOS LEÍDOS
+        partidas_jugadas.setText(rej.getPartidas_jugadas()+"");
+        porcentaje_victorias.setText(rej.getPorcentaje_victorias()+"");
     }
 
 
